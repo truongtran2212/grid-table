f46e9950167d5e7d6c745e9b2334c0a20439d4cd
truongtran2212 <100398436+truongtran2212@users.noreply.github.com>
2023-10-02 09:50:21 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+import React, {
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+  StrictMode,
+} from 'react';
+import { createRoot } from 'react-dom/client';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import './styles.css';
+
+const GridExample = () => {
+  const gridRef = useRef();
+  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+  const [rowData, setRowData] = useState();
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'athlete', width: 150, suppressSizeToFit: true },
+    { field: 'age', width: 50, maxWidth: 50 },
+    { colId: 'country', field: 'country', maxWidth: 300 },
+    { field: 'year', width: 90 },
+    { field: 'date', width: 110 },
+    { field: 'sport', width: 110 },
+    { field: 'gold', width: 100 },
+    { field: 'silver', width: 100 },
+    { field: 'bronze', width: 100 },
+    { field: 'total', width: 100 },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      resizable: true,
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => setRowData(data));
+  }, []);
+
+  const sizeToFit = useCallback(() => {
+    gridRef.current.api.sizeColumnsToFit({
+      defaultMinWidth: 100,
+      columnLimits: [{ key: 'country', minWidth: 900 }],
+    });
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div className="outer-div">
+        <div className="button-bar">
+          <button onClick={sizeToFit}>Size to Fit</button>
+        </div>
+        <div className="grid-wrapper">
+          <div style={gridStyle} className="ag-theme-alpine">
+            <AgGridReact
+              ref={gridRef}
+              rowData={rowData}
+              columnDefs={columnDefs}
+              defaultColDef={defaultColDef}
+              onGridReady={onGridReady}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <GridExample />
+  </StrictMode>
+);
